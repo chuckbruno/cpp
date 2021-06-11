1aa61b876974c5c714f7caf39a2fb5426ba1b1a9
lulongfei <lulongfei@of3d.com>
2021-06-11 10:09:16 +0800

    update str

---
+++ instr3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name:\n";
+    cin.get(name, ArSize).get();
+    cout << "Enter your favorite dessert: \n";
+    cin.get(dessert, ArSize).get();
+
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name << ".\n";
+
+    return 0;
+}
