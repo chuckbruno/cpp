e5c7d980b72aad90c3b78a789c4454adc37b29f3
lulongfei <lulongfei@of3d.com>
2021-06-15 10:30:40 +0800

    update express5.3
    update assign 4.13

--- assign4.13.10.cpp
+++ assign4.13.10.cpp
@@ -5,14 +5,7 @@ int main()
 {
     std::array<float, 3> data;
     std::cout << "Please input three times running record: " << std::endl;
-
-    std::cin >> data[0];
-    std::cin >> data[1];
-    std::cin >> data[2];
-
-    //data.at(0) = firstTime;
-    //data.at(1) = secondTime;
-    //data.at(2) = thirdTime;
+    std::cin >> data[0] >> data[1] >> data[2];
 
     int times = data.size();
     float average = (data[0] + data[1] + data[2]) / times;
