4b0ca33ce631251a4b92b504c6e0186d933873c0
lulongfei <lulongfei@of3d.com>
2021-07-02 21:27:16 +0800

    update codes

---
+++ chapter07/assignment/7.13.9assign.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+
+using namespace std;
+
+const int SLEN = 30;
+
+struct student
+{
+    char fullname[SLEN];
+    char hobby[SLEN];
+    int ooplevel;
+};
+
+
+int getinfo(student pa[], int n);
+void display1(student st);
+void display2(const student *ps);
+void display3(const student pa[], int n);
+
+int main()
+{
+    cout << "Enter class size: ";
+    int class_size;
+    cin >> class_size;
+    while (cin.get() != '\n')
+        continue;
+    student *ptr_stu = new student[class_size];
+    int entered = getinfo(ptr_stu, class_size);
+    for(int i = 0; i < entered; i++)
+    {
+        display1(ptr_stu[i]);
+        display2(&ptr_stu[i]);
+    }
+    display3(ptr_stu, entered);
+    delete[] ptr_stu;
+    cout << "Done\n";
+    return 0;
+}
+
+
+int getinfo(student pa[], int n)
+{
+
+}
