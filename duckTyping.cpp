2d788b9f9895c6ac001ce0ce7f38948cf9f907d2
lulongfei <lulongfei172006@126.com>
2022-03-23 09:03:40 +0800

    update static polymorphism

---
+++ duckTyping.cpp
@@ -0,0 +1,63 @@
+
+#include <chrono>
+#include <iostream>
+
+
+auto start = std::chrono::steady_clock::now();
+
+void writeElapsedTime()
+{
+    auto now = std::chrono::steady_clock::now();
+    std::chrono::duration<double> diff = now - start;
+    std::cerr << diff.count() << " sec, elapsed: ";
+}
+
+
+struct MessageSeverty{
+    void writeMessage() const
+    {
+        std::cerr << "unexpected" << '\n';
+    }
+};
+
+
+struct MessageInformation
+{
+    void writeMessage() const{
+        std::cerr << "information" << '\n';
+    }
+};
+
+struct MessageWarning{
+    void writeMessage() const{
+        std::cerr << "warning" << '\n';
+    }
+};
+
+
+struct MessageFatal:MessageSeverty{};
+
+template<typename T>
+void writeMessage(T& messServer)
+{
+    writeElapsedTime();
+    messServer.writeMessage();
+}
+
+int main()
+{
+    std::cout << '\n';
+
+    MessageInformation messInfo;
+    writeMessage(messInfo);
+
+    MessageWarning messWarn;
+    writeMessage(messWarn);
+
+    MessageFatal messFatal;
+    writeMessage(messFatal);
+
+    std::cout << '\n';
+
+    return 0;
+}
